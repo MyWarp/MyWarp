03d64a3abe545c65b4b2e616526bb17023466c61
TheE <thee@online.de>
2017-04-11 11:10:18 +0200

    Update javadoc deployment for new site structure

--- config/publish-javadoc.sh
+++ config/publish-javadoc.sh
@@ -3,6 +3,9 @@
 # Publish Javadoc of successful CI builds to https://mywarp.github.io/javadoc
 # See https://web.archive.org/web/20150107174657/http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
+destinationBranch="src";
+javadocDestination="source/javadocs/"
+
 if [ "$TRAVIS_REPO_SLUG" == "MyWarp/MyWarp" ] && \
    [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&  \
@@ -10,24 +13,26 @@ if [ "$TRAVIS_REPO_SLUG" == "MyWarp/MyWarp" ] && \
 
   echo -e "Publishing javadoc...\n"
 
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "travis-ci"
-  git clone --quiet --branch=master https://${GH_TOKEN}@github.com/MyWarp/mywarp.github.io $HOME/web > /dev/null
+  git config --global user.email "deploy@travis-ci.org"
+  git config --global user.name "Deployment Bot"
+  git clone --quiet --branch=${destinationBranch} https://${GITHUB_TOKEN}@github.com/MyWarp/mywarp.github.io $HOME/web > /dev/null
   cd $HOME/web
 
-  echo -e "Repository cloned."
+  echo -e "Repository cloned (branch $destinationBranch)."
 
   for module in mywarp-core mywarp-bukkit; do
     mkdir -p ./javadoc/$module
     git rm -rf ./javadoc/$module/*
 
-    cp -Rf $TRAVIS_BUILD_DIR/$module/build/docs/javadoc/ ./javadoc/$module
+    finalDestination=.${javadocDestination}/$module;
+
+    cp -Rf $TRAVIS_BUILD_DIR/$module/build/docs/javadoc/ ${finalDestination}
     git add -f ./javadoc/$module
-    echo -e "Javadocs for '$module' added."
+    echo -e "Javadocs for '$module' copied to ${finalDestination}."
   done
 
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to mywarp.github.io."
-  git push -fq origin master > /dev/null
+  git push -fq origin $destinationBranch > /dev/null
 
   echo -e "Published Javadoc to mywarp.github.io.\n"
 
