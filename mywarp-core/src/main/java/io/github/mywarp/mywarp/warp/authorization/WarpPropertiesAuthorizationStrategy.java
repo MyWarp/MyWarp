ed5f27293b04c85090343b5fc1dd519837be6952
TheE <thee@online.de>
2017-09-23 19:13:02 +0200

    Update Gradle to 4.2, Checkstyle to 8.2 and plugins

--- mywarp-core/src/main/java/io/github/mywarp/mywarp/warp/authorization/WarpPropertiesAuthorizationStrategy.java
+++ mywarp-core/src/main/java/io/github/mywarp/mywarp/warp/authorization/WarpPropertiesAuthorizationStrategy.java
@@ -25,7 +25,7 @@ import io.github.mywarp.mywarp.platform.LocalPlayer;
 import io.github.mywarp.mywarp.warp.Warp;
 
 /**
- * Resolves a user's authentication for a certain warp based on properties of the warp: <ul> <li>A warp is
+ * Resolves a user's authentication for a certain warp based on properties of the warp.<ul> <li>A warp is
  * <i>modifiable</i> if the Actor is the creator,</li> <li>A warp is <i>usable</i> if the entity is the creator, or is
  * an invited player, or the warp is public,</li> <li>A warp is viewable if the Actor is an entity who can use the warp,
  * or the warp is public.</li> </ul>
